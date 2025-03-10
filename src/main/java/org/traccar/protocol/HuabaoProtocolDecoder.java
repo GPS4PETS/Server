c8a644b3734eff64d5dc3fc4fde7c5e2ea0b4d90
GPS4PETS <info@gps4pets.de>
2025-03-10 01:56:28 +0100

    lint clean

--- src/main/java/org/traccar/protocol/HuabaoProtocolDecoder.java
+++ src/main/java/org/traccar/protocol/HuabaoProtocolDecoder.java
@@ -40,7 +40,6 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.HexFormat;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
