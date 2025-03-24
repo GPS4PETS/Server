f80ff5bd8d4ba050cecf72768306e4771e08791e
GPS4PETS <info@gps4pets.de>
2025-03-24 12:32:18 +0100

    omni

--- src/main/java/org/traccar/protocol/OmniProtocolDecoder.java
+++ src/main/java/org/traccar/protocol/OmniProtocolDecoder.java
@@ -13,6 +13,19 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+ /*
+1.3.5 Dimension0 (GPS location report Instructions)
+Locator->Server *TRAR,OM,123456789123456,Dimension0,N,2238.07773,W,11407.55384,6,1.49,1599206439#<LF>
+1 Address sign N S is used to distinguish north latitude and south latitude.
+2 Latitude data
+3 Address sign E W is used to distinguish the east long and west long.
+4 Amenity data
+5 Number of GPS satellites
+6 Horizontal accuracy factor
+7 Timestamp
+Server->Locator No answer
+*/
 package org.traccar.protocol;
 
 import io.netty.channel.Channel;
@@ -33,42 +46,24 @@ public class OmniProtocolDecoder extends BaseProtocolDecoder {
         super(protocol);
     }
 
-    private static final Pattern PATTERN_STANDARD = new PatternBuilder()
-            .groupBegin()
-            .text("$PTMLA,")                     // header
-            .or()
-            .text("%%")                          // header
-            .groupEnd()
+    /* *TRAR,OM,123456789123456,Dimension0,N,2238.07773,W,11407.55384,6,1.49,1599206439# */
+    private static final Pattern PATTERN_POSITION = new PatternBuilder()
+            .text("*TRAR,OM,")                   // header
             .expression("([^,]+),")              // id
-            .expression("([ABCL]),")             // validity
-            .number("(dd)(dd)(dd)")              // date (yymmdd)
-            .number("(dd)(dd)(dd),")             // time (hhmmss)
+            .expression("Dimenson0,")            // Report Name
             .expression("([NS])")
             .number("(dd)(dd.d+)")               // latitude
             .expression("([EW])")
             .number("(d{2,3})(dd.d+),")          // longitude
-            .number("(d+),")                     // speed
-            .number("(d+),")                     // course
-            .number("(?:NA|C(-?d+)),")           // temperature
-            .number("(x{8}),")                   // status
-            .groupBegin()
-            .text("NA")
-            .or()
-            .number("F(d+)")                     // fuel
-            .or()
-            .number("(d+)")                      // card id
-            .groupEnd(",")
-            .number("(d+),")                     // event
             .number("(d+),")                     // satellites
-            .number("(d+.d+),")                  // odometer
-            .number("(d+)")                      // rssi
-            .number(",G(d+)").optional()         // fuel
+            .number("F(d+),")                    // hdop
+            .number("(d+)#")                     // timestamp
             .any()
             .compile();
 
-    private Object decodeStandard(Channel channel, SocketAddress remoteAddress, String sentence) {
+    private Object decodePosition(Channel channel, SocketAddress remoteAddress, String sentence) {
 
-        Parser parser = new Parser(PATTERN_STANDARD, sentence);
+        Parser parser = new Parser(PATTERN_POSITION, sentence);
         if (!parser.matches()) {
             return null;
         }
@@ -81,62 +76,40 @@ public class OmniProtocolDecoder extends BaseProtocolDecoder {
         Position position = new Position(getProtocolName());
         position.setDeviceId(deviceSession.getDeviceId());
 
-        position.setValid(!parser.next().equals("L"));
-        position.setTime(parser.nextDateTime());
         position.setLatitude(parser.nextCoordinate(Parser.CoordinateFormat.HEM_DEG_MIN));
         position.setLongitude(parser.nextCoordinate(Parser.CoordinateFormat.HEM_DEG_MIN));
-        position.setSpeed(UnitsConverter.knotsFromKph(parser.nextInt()));
-        position.setCourse(parser.nextInt());
 
-        position.set(Position.PREFIX_TEMP + 1, parser.next());
-        position.set(Position.KEY_STATUS, parser.nextHexLong());
-        if (parser.hasNext()) {
-            position.set(Position.KEY_FUEL_LEVEL, parser.nextInt() * 0.1);
-        }
-        position.set(Position.KEY_DRIVER_UNIQUE_ID, parser.next());
-
-        int event = parser.nextInt();
-        position.set(Position.KEY_EVENT, event);
-        if (event == 253) {
-            position.set(Position.KEY_IGNITION, true);
-        } else if (event == 254) {
-            position.set(Position.KEY_IGNITION, false);
-        }
-
-        position.set(Position.KEY_SATELLITES, parser.nextInt());
-        position.set(Position.KEY_ODOMETER, parser.nextDouble() * 1000);
-        position.set(Position.KEY_RSSI, parser.nextInt());
-        position.set(Position.KEY_FUEL_LEVEL, parser.nextInt());
+        var sat  = parser.nextInt();
+        position.set(Position.KEY_SATELLITES, sat);
+        position.setValid(sat > 2 ? true : false);
+        position.set(Position.KEY_HDOP, parser.nextDouble() * 1000);
+        position.setTime(new Date(parser.nextInt()));
 
         return position;
     }
 
-    private static final Pattern PATTERN_EXTENDED = new PatternBuilder()
-            .text("$EXT,")                        // header
+    /* *TRAR,OM,123456789123456,H0,A7670C,A011B13A7670ME,80,20,240,300,60,60,1,1,DD:59:DC:F1:18:0B, 5# */
+    private static final Pattern PATTERN_HEARTBEAT = new PatternBuilder()
+            .text("*TRAR,OM,")                   // header
             .expression("([^,]+),")              // id
-            .expression("([ABCL]),")             // validity
-            .number("(dd)(dd)(dd)")              // date (yymmdd)
-            .number("(dd)(dd)(dd),")             // time (hhmmss)
-            .expression("([NS])")
-            .number("(dd)(dd.d+)")               // latitude
-            .expression("([EW])")
-            .number("(d{2,3})(dd.d+),")          // longitude
-            .number("(d+),")                     // speed
-            .number("(d+),")                     // course
-            .number("(?:NA|C(-?d+)),")           // temperature
-            .number("(?:NA|F(d+)),")             // fuel
-            .number("(d+),")                     // satellites
+            .text("H0,")                         // Report Name
+            .expression("([ABCL]),")             // communication modul
+            .expression("([ABCL]),")             // communication modul version
+            .number("(d+)")                      // battery level
             .number("(d+),")                     // rssi
-            .number("(d+.d+),")                  // odometer
-            .number("(?:NA|(d+)),")              // card id
-            .number("(x{8}),")                   // status
-            .number("(d+)")                      // event
+            .number("(d+),")                     // heartbeat interval
+            .number("(d+),")                     // wifi reporting interval
+            .number("(d+),")                     // tracking interval
+            .number("(d+),")                     // mobile detection switch
+            .number("(d+),")                     // moving state
+            .expression("([ABCL]),")             // home wifi mac
+            .number("(d+),")                     // low bat alarm
             .any()
             .compile();
 
-    private Object decodeExtended(Channel channel, SocketAddress remoteAddress, String sentence) {
+    private Object decodeHeartbeat(Channel channel, SocketAddress remoteAddress, String sentence) {
 
-        Parser parser = new Parser(PATTERN_EXTENDED, sentence);
+        Parser parser = new Parser(PATTERN_HEARTBEAT, sentence);
         if (!parser.matches()) {
             return null;
         }
@@ -173,10 +146,10 @@ public class OmniProtocolDecoder extends BaseProtocolDecoder {
             Channel channel, SocketAddress remoteAddress, Object msg) throws Exception {
 
         String sentence = (String) msg;
-        if (sentence.startsWith("%%") || sentence.startsWith("$PTMLA")) {
-            return decodeStandard(channel, remoteAddress, sentence);
-        } else if (sentence.startsWith("$EXT")) {
-            return decodeExtended(channel, remoteAddress, sentence);
+        if (sentence.indexOf(",Dimension0,") > 0) {
+            return decodePosition(channel, remoteAddress, sentence);
+        } else if (sentence.indexOf(",H0,") > 0) {
+            return decodeHeartbeat(channel, remoteAddress, sentence);
         } else {
             return null;
         }
